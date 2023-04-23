002bc6d9be5ee9389802b5e277e7880ffa90afc0
Marcus Rocha <32283039+marcusvrsousa@users.noreply.github.com>
2023-04-22 23:25:00 -0300

    url corrected

--- js/service/service.js
+++ js/service/service.js
@@ -1,27 +1,27 @@
 
 //consumindo API através do axios
 const request = () => {
-    return axios.get('http://makeup-api.herokuapp.com/api/v1/products.json')
+    return axios.get('https://makeup-api.herokuapp.com/api/v1/products.json')
 }
 
 const filterBrand = (brand) =>{
-    return axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brand}`)
+    return axios.get(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=${brand}`)
 }
 
 const filterType = (type) =>{
-    return axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${type}`)
+    return axios.get(`https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${type}`)
 }
 
 const filterPriceGreaterThan = () =>{
-    return axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json?price_greater_than=25`)
+    return axios.get(`https://makeup-api.herokuapp.com/api/v1/products.json?price_greater_than=25`)
 }
 
 const filterPriceLessThan = () =>{
-    return axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json?price_less_than=25`)
+    return axios.get(`https://makeup-api.herokuapp.com/api/v1/products.json?price_less_than=25`)
 }
 
 const filterBestRating = () =>{
-    return axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json?rating_greater_than=3`)
+    return axios.get(`https://makeup-api.herokuapp.com/api/v1/products.json?rating_greater_than=3`)
 }
 
 export const Service = { 
